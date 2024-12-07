26ec84b5ea43724429789e520151103bbfb6b213
shamal melantha <shamalmelantha3@gmail.com>
2024-12-07 11:50:26 +0530

    update ft

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,15 @@
+import flowbitePlugin from 'flowbite/plugin';
+import scrollbarPlugin from 'tailwind-scrollbar';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin'), 
-  require('tailwind-scrollbar'),
-  ]
-
-}
+  plugins: [flowbitePlugin, scrollbarPlugin],
+};
